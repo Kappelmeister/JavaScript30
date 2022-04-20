14d15f72eaad387eba83845f606e5cf7fe66328d
Kappelmeister <sa.guillen00@gmail.com>
2022-04-20 22:04:12 +0200

    drum project changes

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,19 +1,19 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-}
+
+const keys = Array.from(document.querySelectorAll('.key'));//transforming this into array
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
+    if (!audio) return; //stop de function in case there is no keycode element
 
-    key.classList.add('playing');
-    audio.currentTime = 0;
+    audio.currentTime = 0; //rewind to the start so we can hit the key and play instantly
     audio.play();
+    key.classList.add('playing');
 }
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
-
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip if it is not a transform
+    e.target.classList.remove('playing');
+}
